37257c27fa99020306d264bf07cf80fe6379f9fd
Student <student@example.com>
2025-12-06 21:48:06 +0100

    Add meta description to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Konferencijų registracijos ir valdymo sistema">
     <title>@yield('title', __('common.home'))</title>
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
